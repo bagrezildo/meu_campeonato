d81cf1a94d2a61a3de87a8d7f7b2a1875442d78b
bagrezildo <matheusmiguelmedeiros@gmail.com>
2024-01-08 08:48:16 -0300

    criação do componente sidebar

---
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Emitters } from '../emiiters/emitters';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+
+}
