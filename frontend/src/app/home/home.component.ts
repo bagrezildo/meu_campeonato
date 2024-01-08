f0ffccc2d6ba7cb1a5e6c2fae6f8943a36059fbc
bagrezildo <matheusmiguelmedeiros@gmail.com>
2024-01-08 08:53:41 -0300

    definição do componente home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -9,15 +9,16 @@ import { Emitters } from '../emiiters/emitters';
 })
 export class HomeComponent implements OnInit{
   message = '';
+  authenticated = false;
   url: string = 'http://localhost:8000/api';
   constructor(private http:HttpClient){}
 
   ngOnInit():void {
     this.http.get(this.url + '/user', {withCredentials:true}).subscribe(
       (res: any) => {
-        this.message = `Oi ${res.name}`,
+        this.message = `Oi ${res.name}`, this.authenticated = true;
         Emitters.authEmitter.emit(true)},
-      err => {this.message = 'Você não está logado';
+      err => {this.message = 'Você não está logado', this.authenticated = false;
       Emitters.authEmitter.emit(false);
       }
     );
