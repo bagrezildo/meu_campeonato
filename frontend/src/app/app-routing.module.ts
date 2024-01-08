5daf9deb72cde750f3c6a066a0eecb2a78b7b83d
bagrezildo <matheusmiguelmedeiros@gmail.com>
2024-01-08 08:44:50 -0300

    criação das rotas de chamada de api

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { RegistraTimeComponent } from './registra-time/registra-time.component';
+import { TimesComponent } from './times/times.component';
 
 const routes: Routes = [
   {path: '', component : HomeComponent},
   {path: 'login', component : LoginComponent},
   {path: 'register', component : RegisterComponent},
+  {path: 'historico', component : HistoricoComponent},
+  {path: 'campeonato', component : HomeComponent},
+  {path: 'criaTime', component : RegistraTimeComponent},
 ];
 
 @NgModule({
